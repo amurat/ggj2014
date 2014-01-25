adcb64c364821652616f93d1a6fe08f2d21e1be9
rsc390 <rohit@172-30-10-72.dynapool.nyu.edu>
2014-01-25 10:40:28 -0500

    Initial Template

---
+++ constants.js
@@ -0,0 +1,43 @@
+//-----------------
+//GAME CONSTANTS
+//-----------------
+// var TILE_SIZE = 40;
+// var PLAYER_SPEED = 500;
+// var SPEED_MAX = 18;
+// var MAX_LEVEL_HEIGHT=17;
+// var MAX_LEVEL_WIDTH=20;
+
+
+//Game States
+var GAMESTATE_PRELOAD = "preloading",
+	GAMESTATE_MENU = "menu",
+	GAMESTATE_GAMEPLAY = "gameplay",
+  GAMESTATE_INSTRUCTIONS = "instructions",
+  GAMESTATE_END = "end";
+
+		
+
+
+//-----------------
+//MAPS
+//-----------------
+
+// //slide as a single entity
+// var levelStart = {w:9,h:8};
+// levelStart.map = [
+//     '#','#','#','#','#','#','#','#','#',
+    
+//     '#','p','P','#',' ',' ',' ',' ','#',
+    
+//     '#',' ',' ','#',' ',' ',' ',' ','#',
+    
+//     '#',' ',' ',' ',' ',' ',' ',' ','#',
+    
+//     '#','#','#',' ',' ',' ','#','#','#',
+    
+//     '#','a','b',' ',' ',' ',' ',' ','#',
+    
+//     '#','#','#','#',' ',' ',' ',' ','#',
+    
+//     '#','#','#','#','#','#','#','#','#'
+// ];
---
+++ game.js
@@ -0,0 +1,242 @@
+(function(document) {
+
+var game = new Phaser.Game(800, 690, Phaser.CANVAS, '', { preload: preload, create: create, update: update, render: render});
+
+
+  // - - - - - - - - - - - - - - - //
+ // - - - - - PRELOADING - - - - -//
+// - - - - - - - - - - - - - - - //
+
+//PHASER - Preload assets
+function preload() {
+	//LOAD STUFF
+  //game.load.tilemap('level1', 'bnb_map1TEST.json', null, Phaser.Tilemap.TILED_JSON);
+	
+  //LOAD IMAGES
+	// game.load.image('block', 'images/block2.png');
+
+	//LOAD SOUNDS
+	// game.load.audio('thunk','sound/blop_2.wav');
+}
+
+
+
+
+
+
+
+  // - - - - - - - - - - - - - - - //
+ //- - - - INITIALIZE GAME - - - -//
+// - - - - - - - - - - - - - - - //
+
+
+//- - VARIABLES - - //
+//Set up all important game variables (stubs)
+var debugging;
+
+//PHASER - Initialize Game
+function create() {
+	//Initiate all starting values for important variables/states/etc 
+  debugging = false;
+
+
+  // - - - RENDERING - - - //
+  // levelText = game.add.text(LEVEL_TEXT_OFFSET,UI_TEXT_HEIGHT,"1", STYLE_HUD);
+
+  currentScreen = game.add.sprite(0,0,'imageTitle');
+  
+  //Add Input Handlers
+  document.addEventListener("keydown",keyDownHandler, false);
+	document.addEventListener("keyup",keyUpHandler, false);
+}
+
+
+
+  // - - - - - - - - - - - - - - - //
+ //- - - - - GAME LOOP   - - - - -//
+// - - - - - - - - - - - - - - - //
+
+//PHASER - Game Loop
+function update() 
+{
+  //Enable DEBUG mode
+  if(keyJustPressed("I")){
+    debugging = !debugging;
+  }
+
+  //Choose correct state!
+	if (gameState == GAMESTATE_GAMEPLAY)
+	{
+		//User Input
+		updateGameInput();
+		
+		//Game Logic	
+		updateGameLogic();
+    
+    // if(gameState == GAMESTATE_END){
+    //   drawGameOver();
+    // }
+	}
+  else if(gameState == GAMESTATE_END)
+  {
+    updateGameOverInput();
+    
+    // if(gameState == GAMESTATE_MENU){
+    //   drawMenu();
+    // }
+  }
+
+  //ROUND all values (to fix stupid phaser physics stuff)
+  // heroSmart.body.x = Math.round(heroSmart.body.x);
+  // heroSmart.body.y = Math.round(heroSmart.body.y);
+
+	//clean out all saved input fields!
+	clearInput();
+}
+
+function updateGameLogic(modifier)
+{
+	//Deal with collision, move objects, etc
+}
+
+  // - - - - - - - - - - - - - - - //
+ //- - - -  PROCESS INPUT- - - - -//
+// - - - - - - - - - - - - - - - //
+
+function updateGameInput()
+{
+  // if(keyJustPressed("R")){
+  //   //do something
+  // }
+}
+
+function updateGameOverInput()
+{
+}
+
+
+  // - - - - - - - - - - - - - - - //
+ //- - - - - -RENDERING  - - - - -//
+// - - - - - - - - - - - - - - - //
+function render()
+{
+  if(debugging){
+    // game.debug.renderSpriteCoords(heroSmart,30,150);
+
+    // game.debug.renderSpriteCollision(heroSmart,30,550);
+    
+    // game.debug.renderSpriteBody(heroSmart);
+    
+    // game.debug.renderText("FPS: " + game.time.fps,100,100,"#FFFFFF","14px Courier");
+  }
+}
+
+
+  // - - - - - - - - - - - - - - - //
+ //- - - - LEVEL FUNCTIONS - - - -//
+// - - - - - - - - - - - - - - - //
+
+//unloads the current level + loads the next level in the array
+function nextLevel()
+{
+}
+
+//Given an index, load a SPECIFIC level
+function selectLevel(index)
+{
+}
+
+//Reset all level variables, clear content, and reload the content
+function resetLevel()
+{
+}
+
+//EMPTY THE LEVEL
+function clearLevel()
+{
+}
+
+
+
+  // - - - - - - - - - - - - - - - //
+ //- - - - -HANDLE INPUT - - - - -//
+// - - - - - - - - - - - - - - - //
+var keysDown = {};
+var keysPressedThisFrame = [];
+
+function keyDownHandler(e)
+{
+  if(!(e.keyCode in keysDown)){
+    keysDown[e.keyCode] = 1; //just pressed
+    //keysPressedThisFrame.push(e.keyCode);
+  }
+  if (e.keyCode == 13 || e.keyCode == 32 || e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 
+			|| e.keyCode == 40) 
+	{
+    e.preventDefault();
+  }
+}
+
+function keyUpHandler(e)
+{
+  delete keysDown[e.keyCode];
+}
+
+function keyIsDown(target)
+{
+  var keyCode = target.charCodeAt(0);
+  
+  if(keyCode in keysDown){
+    return true;
+  }
+  
+  return false;
+}
+
+function keyJustPressed(target)
+{
+  var keyCode = target.charCodeAt(0);
+  
+  if(keyCode in keysDown){
+    if(keysDown[keyCode] == 1){
+      return true;
+    }
+  }
+  
+  return false;
+}
+
+function keyCodeJustPressed(keyCode)
+{
+  if(keyCode in keysDown){
+    if(keysDown[keyCode] == 1){
+      return true;
+    }
+  }
+  
+  return false;
+}
+
+function anyKeyJustPressed()
+{
+  for(var key in keysDown)
+  {
+    if(keysDown.hasOwnProperty(key)){
+      if(keysDown[key]==1)
+        return true;
+    }
+  }
+
+  return false;
+}
+
+function clearInput()
+{
+  for(var keyCode in keysDown){
+    if(keysDown[keyCode] == 1){
+      keysDown[keyCode] = 2;
+    }
+  }
+}
+
+})(document);
